4491713263b4e3180fb054d21d2ade094a36e3ab
LelesBox <li_xiaoliang@foxmail.com>
2016-03-03 22:48:31 +0800

    测试push

--- controller/handleHook.js
+++ controller/handleHook.js
@@ -5,6 +5,14 @@ module.exports = {
     //更简单的方法快速填写路由方法
     "get /test": function *() {
         console.log("ASDSAD")
+        console.log(this.request)
+        this.body = "hello test"
+    },
+    "post /test": function *() {
+        console.log("request")
+        console.log(this.request)
+        console.log("body")
+        console.log(this.request.body)
         this.body = "hello test"
     }
 }
